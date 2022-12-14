ebb6540a68314f3971d172414fcd666bfe1c528c
abdulquadir77 <abdulquadir77@yahoo.com>
2022-12-15 00:56:58 +0530

    added

---
+++ frontend/src/Components/Abdul/Caraousel.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { useState } from "react";
+import { Flex, Box, Text, HStack, Image } from "@chakra-ui/react";
+
+const Caraousel = () => {
+  const arrowStyles = {
+    cursor: "pointer",
+    pos: "absolute",
+    top: "50%",
+    w: "auto",
+    mt: "-22px",
+    p: "16px",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "18px",
+    transition: "0.6s ease",
+    borderRadius: "0 3px 3px 0",
+    userSelect: "none",
+    _hover: {
+      opacity: 0.8,
+      bg: "black",
+    },
+  };
+  const slides = [
+    {
+      img: "https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner1-12Dec2022A.gif",
+    },
+    {
+      img: "https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner6-08Dec2022.gif",
+    },
+    {
+      img: "https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner2-05Dec2022.jpg",
+    },
+    {
+      img: "https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner4-05Dec2022.jpg",
+    },
+    {
+      img: "https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/LS-Uber-HP-Desktop-HeroBanner3-05Dec2022.jpg",
+    },
+  ];
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slidesCount = slides.length;
+
+  const prevSlide = () => {
+    setCurrentSlide((s) => (s === 0 ? slidesCount - 1 : s - 1));
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide((s) => (s === slidesCount - 1 ? 0 : s + 1));
+  };
+
+  const setSlide = (slide) => {
+    setCurrentSlide(slide);
+  };
+
+  const carouselStyle = {
+    transition: "all .8s",
+    ml: `-${currentSlide * 100}%`,
+  };
+  return (
+    <>
+      <Flex
+        width={{ base: "100%", sm: "100%", md: "80%" }}
+        margin="auto"
+        bg="#edf3f8"
+        mt="40px"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Flex w="full" overflow="hidden" pos="relative">
+          <Flex h="450px" w="full" {...carouselStyle}>
+            {slides.map((slide, sid) => (
+              <Box key={`slide-${sid}`} boxSize="full" shadow="md" flex="none">
+                <Text
+                  color="white"
+                  fontSize="xs"
+                  p="8px 12px"
+                  pos="absolute"
+                  top="0"
+                >
+                  {sid + 1} / {slidesCount}
+                </Text>
+                <Image
+                  src={slide.img}
+                  alt="carousel image"
+                  boxSize="full"
+                  backgroundSize="cover"
+                />
+              </Box>
+            ))}
+          </Flex>
+          <Text {...arrowStyles} left="0" onClick={prevSlide}>
+            &#10094;
+          </Text>
+          <Text {...arrowStyles} right="0" onClick={nextSlide}>
+            &#10095;
+          </Text>
+          <HStack justify="center" pos="absolute" bottom="8px" w="full">
+            {Array.from({
+              length: slidesCount,
+            }).map((_, slide) => (
+              <Box
+                key={`dots-${slide}`}
+                cursor="pointer"
+                boxSize={["7px", null, "15px"]}
+                m="0 2px"
+                bg={
+                  currentSlide === slide ? "blackAlpha.800" : "blackAlpha.500"
+                }
+                rounded="50%"
+                display="inline-block"
+                transition="background-color 0.6s ease"
+                _hover={{
+                  bg: "blackAlpha.800",
+                }}
+                onClick={() => setSlide(slide)}
+              ></Box>
+            ))}
+          </HStack>
+        </Flex>
+      </Flex>
+    </>
+  );
+};
+
+export default Caraousel;
