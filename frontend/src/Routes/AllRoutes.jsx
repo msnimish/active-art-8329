a2bc9b3bce83b077393e0cf0a20778db946c6577
rohit0985 <rohitsharma4681@gmail.com>
2022-12-17 18:01:20 +0530

    added

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -9,7 +9,7 @@ import Home from "../Pages/Home"
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}>Home</Route>
+      <Route path='/'  element={<Home/>}>Home</Route>
       <Route path='/p' element={<Products/>}>Products</Route>
       <Route path='/s' element={<SingleProduct/>}>SingleProduct</Route>
     </Routes>
