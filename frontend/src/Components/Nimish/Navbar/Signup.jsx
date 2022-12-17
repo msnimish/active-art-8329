2960ec9c74336e8b87170b3c3e4164c85bb5cd28
msnimish <msnimish24@gmail.com>
2022-12-17 15:49:41 +0530

    Day-4 End fw20_0715

--- frontend/src/Components/Nimish/Navbar/Signup.jsx
+++ frontend/src/Components/Nimish/Navbar/Signup.jsx
@@ -1,73 +1,117 @@
-import { Button } from '@chakra-ui/button'
-import { FormControl } from '@chakra-ui/form-control'
-import { useDisclosure } from '@chakra-ui/hooks'
-import { Input } from '@chakra-ui/input'
-import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/modal'
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import Login from './Login'
-import axios from "axios"
+import { Button } from "@chakra-ui/button";
+import { FormControl } from "@chakra-ui/form-control";
+import { useDisclosure } from "@chakra-ui/hooks";
+import { Input } from "@chakra-ui/input";
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/modal";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Login from "./Login";
+import axios from "axios";
+import { BASE_URL } from "../../../url";
 
 const LoginWrapper = styled.div`
-
-font-family: "Proxima Nova"
-
-.SignUpBtn{
+  font-family: "Proxima Nova" .SignUpBtn {
     border-left: 1px solid #c7c7c7;
     border-radius: 0px;
-}
-.SignUpBtn:hover{
+  }
+  .SignUpBtn:hover {
     background-color: transparent;
-    color: #FAA619
-}
-`
+    color: #faa619;
+  }
+`;
 
 const Signup = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const [ email, setEmail ] = useState("");
-    const [ password, setPassword ] = useState("");
-    const [ name, setName ] = useState("");
-    const [ signupStatus, setSignupStatus ] = useState("Already have an account?");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [data, setData] = useState({});
+  const [signupStatus, setSignupStatus] = useState(false);
+  console.log(BASE_URL);
 
-    const handleSubmit = (e) => {
-      e.prevent.default();
-      
-    }
+  const handleChange= (e)=>{
 
-    useEffect(() => {
-        axios.post("/signup", (req,res)=>{
+  }
 
-        })
-    }, []);
+  const handleSubmit = async(e) => {
+    e.prevent.default();
+    let response = await axios.post(`${BASE_URL}/signup`,data);
+    alert(response.message);
+    setSignupStatus(true);
+
+  };
+
+  useEffect(() => {
+    axios.post(`/signup`, (req, res) => {});
+  }, []);
 
   return (
     <LoginWrapper>
-        <Button onClick={ onOpen } fontFamily={"Proxima Nova"} bg={"none"} fontSize="1em" className="SignUpBtn">Sign Up / Sign In</Button>
+      <Button
+        onClick={onOpen}
+        fontFamily={"Proxima Nova"}
+        bg={"none"}
+        fontSize="1em"
+        className="SignUpBtn"
+      >
+        Sign Up / Sign In
+      </Button>
 
-        <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent borderRadius="0px">
           <ModalHeader>SIGN UP</ModalHeader>
           <ModalCloseButton />
-            <ModalBody display="flex" flexDirection="column" gap="10px">
-              <form onSubmit = {(e)=>handleSubmit(e)} id="signupForm">
-              <FormControl>
-                <Input fontFamily="Proxima Nova" placeholder='Full Name' borderRadius={"0"} value={name} onChange={(e)=> setName(e.target.value)}/>
-                <Input fontFamily="Proxima Nova" placeholder='Email' borderRadius={"0"} value={email}  onChange={(e)=> setEmail(e.target.value)}/>
-                <Input fontFamily="Proxima Nova" placeholder='Password (More than 8 characters)' borderRadius={"0"} value={password} onChange={(e)=> setPassword(e.target.value)}/>
+          <ModalBody display="flex" flexDirection="column" gap="10px">
+            <form onSubmit={(e) => handleSubmit(e)} id="signupForm">
+              <FormControl display="flex" flexDirection="column" gap="10px">
+                <Input
+                  fontFamily="Proxima Nova"
+                  placeholder="Full Name"
+                  borderRadius={"0"}
+                  name="name"
+                  onChange={(e) => handleChange(e)}
+                />
+                <Input
+                  fontFamily="Proxima Nova"
+                  placeholder="Email"
+                  borderRadius={"0"}
+                  name="email"
+                  onChange={(e) => handleChange(e)}
+                />
+                <Input
+                  fontFamily="Proxima Nova"
+                  placeholder="Password (More than 8 characters)"
+                  borderRadius={"0"}
+                  name="password"
+                  onChange={(e) => handleChange(e)}
+                />
               </FormControl>
             </form>
-            </ModalBody>
+          </ModalBody>
           <ModalFooter>
-            <Button type="submit" bg="#Faa619" mr={3} borderRadius="0" color="white" _hover={{bg:"#FAA619", color:"black", transition:"0.1s"}} form="signupForm" >
+            <Button
+              type="submit"
+              bg="#Faa619"
+              mr={3}
+              borderRadius="0"
+              color="white"
+              _hover={{ bg: "#FAA619", color: "black", transition: "0.1s" }}
+              form="signupForm"
+            >
               SIGN UP
             </Button>
-            <Login message={signupStatus} onClick={ onClose }/>
+            <Login status={signupStatus} closeSignup={onClose} />
           </ModalFooter>
         </ModalContent>
       </Modal>
     </LoginWrapper>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
